4b94546c2ceb83e40aec5067be7df58d266cd734
林雪峰 <1317711934@qq.com>
2024-04-05 09:47:05 +0800

    Update uart_send.py

--- uart_send.py
+++ uart_send.py
@@ -13,39 +13,16 @@ fm.register(7, fm.fpioa.UART1_TX, force=True)
 # 初始化串口
 uart = UART(UART.UART1, 115200, read_buf_len=4096)
 lcd.init(type=1,freq=15000000,color=lcd.WHITE)
-version_num = "version:3.3.0"
+version_num = "version:4.2.0"
 lcd.draw_string(0,0,version_num)
 lcd.clear()
 #PWM通过定时器配置，接到IO9引脚
 tim0 = Timer(Timer.TIMER1, Timer.CHANNEL0, mode=Timer.MODE_PWM)
-beep = PWM(tim0, freq=1, duty=10, pin=9)
+
 formatted_time = ""
 total_its = 0
 hit_its = 0
-
-class StringMerger:
-    def __init__(self):
-        self.queue = []  # 用于存储待合并的字符串
-        self.count = 0  # 用于跟踪还需要保留多少字符串
-
-    def receive_string(self, s):
-        if 'r' in str(s):  # 检查当前字符串中是否包含'r'
-            self.count = 3   # 包括当前字符串在内，还需要合并3个字符串
-            self.queue.append(s.replace('r', ''))  # 添加到队列之前去掉'r'
-        elif self.count > 0:
-            self.queue.append(s)  # 如果当前字符串不包含'r'，直接添加到队列中
-            self.count -= 1  # 减少需要合并的字符串数量
-            # print(self.count)
-            # print(self.queue)
-            if self.count == 0:
-                # 当计数器减到0时，进行合并，并清空队列
-                merged_string = ''.join(self.queue)  # 合并字符串
-                self.queue.clear()  # 清空队列
-                return merged_string  # 返回合并后的字符串
-        # 如果当前字符串不包含'r'，且不在合并范围内，则返回None
-        return None
-
-merger = StringMerger()
+risk_status = False
 
 def get_time(arg = 0):
     # 获取当前时间戳（Unix时间戳，自1970年1月1日以来的秒数）
@@ -67,13 +44,26 @@ def get_time(arg = 0):
         )
         #print(formatted_time)
 
-def print_hit(arg=0):
-    lcd.draw_string(180,20,"Hit Rate:" + str(hit_its) +"/" + str(total_its) ,lcd.GREEN)
+#def print_hit(arg=0):
+    #lcd.draw_string(180,20,"Hit Rate:" + str(hit_its) +"/" + str(total_its) ,lcd.GREEN)
 
+def tim_thread(arg=0):
+    beep = PWM(tim0, freq=1, duty=10, pin=9)
+    beep.enable()
+    global risk_status
+    lcd.draw_string(180,20,"Hit Rate:" + str(hit_its) +"/" + str(total_its) ,lcd.GREEN)
+    if (risk_status):
+        beep.freq(1200)
+    else:
+        beep.disable()
+        beep.deinit()
 
 get_time()
 tim2 = Timer(Timer.TIMER2, Timer.CHANNEL0, mode=Timer.MODE_PERIODIC, period=1000, callback=get_time)
-tim3 = Timer(Timer.TIMER2, Timer.CHANNEL1, mode=Timer.MODE_PERIODIC, period=2000, callback=print_hit)
+tim3 = Timer(Timer.TIMER2, Timer.CHANNEL1, mode=Timer.MODE_PERIODIC, period=2000, callback=tim_thread)
+
+
+
 
 # 定义一个字典用于保存解析后的数据
 data_dir = {}
@@ -113,6 +103,7 @@ def calculate_weight(value):
 
 
 def calculate_risk(data):
+    global risk_status
     # 从raw_data提取相关参数
     temperature = data['params']['temperature']
     smoke = data['params']['smoke']
@@ -123,7 +114,7 @@ def calculate_risk(data):
     # 将所有参数映射到0-1之间
     temperature_normalized = temperature / 50
     smoke_normalized = smoke / 8
-    CO_normalized = CO / 12
+    CO_normalized = CO / 20
     dry_normalized = dry / 80
 
     # 依次计算参数对风险度的贡献，其中取值不超过1的参数权重为1，超过1的参数权重为3
@@ -136,8 +127,12 @@ def calculate_risk(data):
     risk = (tem_contrib + smoke_contrib + CO_contrib + dry_contrib) / 4
 
     # 如果risk大于1，说明有参数超过了正常范围，风险度为1
-    if risk > 1:
+    if risk > 1 or data['params']['FIRE'] == 1:
         risk = 1
+        risk_status = True
+    else:
+        risk /= 10
+        risk = round(risk,4)
 
     # 返回风险度
     return risk
@@ -150,10 +145,11 @@ END
 
 
 def send_json_data(timer = None):
-    beep.enable()
+    #beep.enable()
 
     global data_dir
     global data_2_send
+    data_2_send["params"]["Risk"] = round(data_2_send["params"]["Risk"],3)
     # 新建一个空字典用于存储转换后的内容
     data_send_ready = {"params": {}}
 
@@ -197,7 +193,7 @@ def send_json_data(timer = None):
 
     lcd.draw_string(0,220,str(int(time.time())) + "  :  =====Send Succeed=====",lcd.GREEN)
     #beep.freq(800)
-    beep.disable()
+    #beep.disable()
 
 # 发送一次JSON数据
 send_json_data()
@@ -224,7 +220,7 @@ while True:
     #print("UART NULL")
 
     if text:  # 如果读取到了数据
-        beep.enable()
+        #beep.enable()
         try:
             received_data = text.decode('utf-8')
         except ValueError:
@@ -244,23 +240,28 @@ while True:
                     # 更新本地数据中对应键的值
                     data_2_send["params"][key] = value["value"]
 
-            '''
-            拼接图像
-            '''
-            red_cache = merger.receive_string(data_2_send["params"]["red"])
-            if red_cache != None:
-                #print("Red Cache: ",red_cache)
-                #print("\n")
-                print("Red Cache length: ",len(red_cache))
-                print("\n")
+            #计算并修改风险度
+            data_2_send["params"]["Risk"] = calculate_risk(data_2_send)
+            if (data_2_send["params"]["Risk"] == 1):
+                risk_status = True
+            else:
+                risk_status = False
 
+             #修改fire数据
+            if (data_2_send["params"]["FIRE"] > 30):
+                data_2_send["params"]["FIRE"] = 1
+                data_2_send["params"]["Risk"] = 1
+                risk_status = True
+            else:
+                data_2_send["params"]["FIRE"] = 0
+                risk_status = False
 
-
-
-            '''
-            刷新接收到的数据
-            '''
-
+            #if (risk_status):
+                #beep.freq(1200)
+                ##print('beep')
+            #else:
+                #beep.disable()
+                #beep.freq(0)
 
             # 获取params部分
             params = data_2_send["params"]
@@ -285,29 +286,12 @@ while True:
                     y_start += 20
                     lcd.draw_string(0, y_start, line_text, lcd.WHITE)
             hit_its += 1
-            '''
-            END
-            '''
-
-            #计算并修改风险度
-            data_2_send["params"]["Risk"] = calculate_risk(data_2_send)
-            #risk = data_2_send["params"]["Risk"]
-            #print(risk)
-            #if (risk>0.5):
-                #beep.freq(1200)
-                #print('beep')
 
             #print('\n')
         except ValueError:
             print("Invalid JSON format received.")
 
-        # 回传接收到的数据（或JSON格式的数据）
-        #uart.write(("I got your data: " + received_data).encode())
-
-        #beep.freq(1200)
-
-        #utime.sleep_ms(200)  # 响200毫秒
-        beep.disable()
+        #beep.disable()
         lcd.draw_string(120,0,str(int(time.time())) + ":***Had Receviced***",lcd.GREEN)
 
         #utime.sleep_ms(1000)
